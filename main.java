43ebe785f0c4b8b25930a345258a0f3148caab1f
Firuzjon <firuzjon@vestberry.com>
2022-02-16 16:22:54 +0100

    new changes

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;  
 class main{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
+    public static void main(String [] args){ 
+    double cislo = 30/20f;
+    Scanner number = new Scanner(System.in);
+    int inputNumber = number.nextInt();
+    System.out.println(inputNumber);  
     }  
 }  
